5c5614d69b1b0cd86844e31a17a016075556e743
Andy Swinburne <Andrew_Swinburne@next.co.uk>
2024-11-14 16:47:16 +0000

    feat(dasboard): add wrapper and navbar

---
+++ client/src/app/DashboardWrapper.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import NavBar from "@/app/(components)/NavBar"
+
+const DashboardWrapper = ({children}: {children: React.ReactNode}) => {
+    return (
+        <div className="light flex bg-gray-50 text-gray-900 w-full min-h-screen">
+            Sidebar
+            <main className="flex flex-col w-full h-full py- px-9 bg-gray-50 md:pl-24">
+                <NavBar />
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export default DashboardWrapper
