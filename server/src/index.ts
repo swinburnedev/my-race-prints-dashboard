8b7f8027e4c8fce6457127ae86b36251b555e129
Andy Swinburne <andy@swinburne.dev>
2024-11-22 13:58:29 +0000

    feat: express server

---
+++ server/src/index.ts
@@ -0,0 +1,30 @@
+import express from "express"
+import dotenv from "dotenv"
+import bodyParser from "body-parser"
+import cors from "cors"
+import helmet from "helmet"
+import morgan from "morgan"
+
+// Route imports
+
+// Configurations
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}))
+app.use(morgan("common"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
+
+// Routes
+app.get("/hello", (req, res) => {
+    res.send("Hello World")
+})
+
+// Server
+const port = process.env.PORT || 3001
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
