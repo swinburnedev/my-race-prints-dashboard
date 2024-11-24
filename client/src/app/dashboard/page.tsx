ec431d29e89bf2e7a7e98b9f1ec0e37fc4bd111d
Andy Swinburne <andy@swinburne.dev>
2024-11-24 19:30:15 +0000

    feat(dashboard): skeleton

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,9 +1,19 @@
-import React from "react"
+"use client"
 
 type Props = {}
 
 const Dashboard = (props: Props) => {
-    return <div>Dashboard</div>
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 xs:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+            <div className="bg-gray-500 row-span-3 xl:row-span-6"></div>
+            <div className="bg-gray-500 row-span-3 xl:row-span-6"></div>
+            <div className="bg-gray-500 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1"></div>
+            <div className="bg-gray-500 row-span-3"></div>
+            <div className="bg-gray-500 md:row-span-1 xl:row-span-2"></div>
+            <div className="bg-gray-500 md:row-span-1 xl:row-span-2"></div>
+            <div className="bg-gray-500 md:row-span-1 xl:row-span-2"></div>
+        </div>
+    )
 }
 
 export default Dashboard
